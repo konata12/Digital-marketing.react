1319daf5ce5d90eabb058cc784d3d564fab5c046
Kekish Dima <dimakeshaa@gmail.com>
2023-04-11 23:29:45 +0300

    aboba

--- digital-marketing/src/index.js
+++ digital-marketing/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+console.dir(1)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
